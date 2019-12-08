fa127e667a758713da8b6ed1352400f4c6f31823
komal Ahire <pooja@navgurukul.lan>
2019-12-08 12:10:38 +0530

    change src

--- src/Hangman.js
+++ src/Hangman.js
@@ -7,107 +7,94 @@ import image3 from "./Images/3.jpg";
 import image4 from "./Images/4.jpg";
 import image5 from "./Images/5.jpg";
 import image6 from "./Images/6.jpg";
-
-const items = ["python", "javascript", "mongodb", "java", "html", "css", "c",  "kotlin", "php", "ruby"];
+const items = ["python", "html", "css", "java",];
 const images = [image0, image1, image2, image3, image4, image5, image6];
 
-const newGuessWord = () => items[Math.floor(Math.random() * items.length)];
+let newGuessWord = () => items[Math.floor(Math.random() * items.length)];
 var alphabets = "abcdefghijklmnopqrstuvwxyz".split("");
 
 class HangmanGame extends Component {
-    constructor(props) {
-        super(props);
-        this.state = getInitialState();
-    }
+	constructor(props) {
+		super(props);
+		var a = newGuessWord()
+		this.state = {
+			lifes: images.length - 1,
+			secretWord:a,
+			Left_word:a,
+			alreadySelectedAlphabets: "",
+			// filterWord: newGuessWord(),
+		}
+	};
+	handleAplhabetClick = alphabet => {
+		this.setState(prevState => ({
+			alreadySelectedAlphabets: prevState.alreadySelectedAlphabets + alphabet,
+			lifes: this.state.secretWord.includes(alphabet) ? prevState.lifes : prevState.lifes - 1,
+			Left_word:words,
+			b:words.length
+			,
+		}));
+	var words = this.state.Left_word.replace(new RegExp(alphabet, 'g'), '');
+	
+	}
 
-    handleAplhabetClick = alphabet => {
-        if (this.shouldNotBeAbleToPlay()) {
-            return;
-        }
-        this.setState(prevState => ({
-            alreadySelectedAlphabets: prevState.alreadySelectedAlphabets + alphabet,
-            lifes: guessIsWrong(
-                prevState.secretWord,
-                alphabet,
-                prevState
-            )
-        }));
-    };
+	handleReset = () => {
+		var a = newGuessWord()
+		this.setState({
+			lifes: images.length - 1,
+			secretWord:a,
+			alreadySelectedAlphabets: "",
+			Left_Word:a,
+			b:a.length
+		});
+	};
 
-    handleReset = () => {
-        this.setState(getInitialState());
-    };
-
-    shouldNotBeAbleToPlay() {
-        return this.state.lifes <= 0;
-    }
-
-    render() {
-        const NO_KEYBOARD_TO_PLAY = null;
-        const secretWordLetterList = this.state.secretWord.split("");
-        return (
-            <div>
+	render() {
+		const secretWordLetterList = this.state.secretWord.split("");
+		
+		return (
+			<div>
                 <p className="kom">  Guesses Left : {this.state.lifes}</p>
-                <div className="center">
-                    <img
-                        src={images[this.nextImageToBeShown()]}
-                        className="img_cenrer"
-                        alt="image1"
-                    />
-                    <p className="text">Guess the Programming Language ?</p>
-                    {secretWordLetterList.map(alphabet =>
-                        this.selectedAplhabet(alphabet) ||
-                            this.shouldNotBeAbleToPlay() ? (
-                                <span> {alphabet} </span>
-                            ) : (
-                                <span> -</span>
-                            )
-                    )}
-                </div>
-                <div id="button_set">
-                {this.state.lifes === 0 ? <h1 className="lostGame">you Lost the game</h1> : null}
-                
-                    {this.shouldNotBeAbleToPlay()
-                        ? NO_KEYBOARD_TO_PLAY
-                        : alphabets.map(alphabet => (
-                            <button
-                                onClick={() => this.handleAplhabetClick(alphabet)}
-                                className="set_alphabate"
-                                disabled={this.selectedAplhabet(alphabet)}
-                            >
-                                {alphabet}
-                            </button>
-                        ))}
-                    <br />
-                    <button onClick={this.handleReset} className="set_alphabate buttons">
-                        reset
-                    </button>
-                </div>
-            </div>
-        );
-    }
+				<div className="center">
+					<img
+						src={images[images.length - this.state.lifes - 1]}
+						className="image_center"
+						alt="image0"
+					/>
+					<p className="text">Guess the Programming Language ?</p>
+					{secretWordLetterList.map(alphab =>
+						this.state.alreadySelectedAlphabets.includes(alphab) ||
+							this.state.lifes <= 0 ? (
+								<span> {alphab} </span>
+							) : (
+								<span> _ </span>
+							)
+					)}
 
-    selectedAplhabet(alphabet) {
-        return this.state.alreadySelectedAlphabets.includes(alphabet);
-    }
+				</div>
 
-    nextImageToBeShown() {
-        return images.length - 1 - this.state.lifes;
-    }
+				<div id="button_set">
+				{this.state.lifes === 0 ? <p className="lostGame">You Lost</p> :null}
+				{this.state.b === 0 ? <p className="wonGame">you won</p>:null }
+					{this.state.lives === 0 || this.state.b === 0
+					? null
+						
+					: alphabets.map(alphabet => (
+							<button
+								onClick={() => this.handleAplhabetClick(alphabet)}
+								className="set_alphabate"
+								disabled={this.state.alreadySelectedAlphabets.includes(alphabet)}>
+								{alphabet}
+							</button>
+						))}
+
+					<br />
+					<button onClick={this.handleReset} className="set_alphabate buttons">
+						reset
+					</button>
+
+				</div>
+			</div>
+		);
+	}
 }
-
-function getInitialState() {
-    return {
-        lifes: images.length - 1,
-        secretWord: newGuessWord(),
-        alreadySelectedAlphabets: ""
-    };
-}
-
-function guessIsWrong(secretWord, secretLetter, prevState) {
-    return secretWord.includes(secretLetter)
-        ? prevState.lifes
-        : prevState.lifes - 1;
-}
-
 export default HangmanGame;
